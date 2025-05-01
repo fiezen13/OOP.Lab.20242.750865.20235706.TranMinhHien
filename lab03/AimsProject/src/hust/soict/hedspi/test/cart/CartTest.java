f10e788b67bade113884a9afec7db84618148556
fiezen13 <hawkins1302@gmail.com>
2025-05-01 10:20:54 +0700

    finish class diagram

--- lab03/AimsProject/src/hust/soict/hedspi/test/cart/CartTest.java
+++ lab03/AimsProject/src/hust/soict/hedspi/test/cart/CartTest.java
@@ -1,6 +1,5 @@
 package hust.soict.hedspi.test.cart;
 import hust.soict.hedspi.aims.cart.*;
-import hust.soict.hedspi.aims.disc.*;
 import hust.soict.hedspi.aims.media.DigitalVideoDisc;
 
 public class CartTest {
@@ -9,17 +8,17 @@ public class CartTest {
         Cart cart = new Cart();
 
         // Create new DVD objects and add them to the cart
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc(0, "The Lion King",
                                                      "Animation", "Roger Allers", 87, 19.95f);
-        cart.addDigitalVideoDisc(dvd1);
+        cart.addMedia(dvd1);
 
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc(0, "Star Wars",
                                                      "Science Fiction", "George Lucas", 87, 24.95f);
-        cart.addDigitalVideoDisc(dvd2);
+        cart.addMedia(dvd2);
 
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
-                                                     "Animation", 18.99f);
-        cart.addDigitalVideoDisc(dvd3);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc(0, "Aladin",
+                                                     "Animation", null, 0, 18.99f);
+        cart.addMedia(dvd3);
 
         // Test the print method
         cart.print();
--- lab03/AimsProject/src/hust/soict/hedspi/test/store/StoreTest.java
+++ lab03/AimsProject/src/hust/soict/hedspi/test/store/StoreTest.java
@@ -1,6 +1,4 @@
 package hust.soict.hedspi.test.store;
-
-import hust.soict.hedspi.aims.disc.*;
 import hust.soict.hedspi.aims.media.DigitalVideoDisc;
 import hust.soict.hedspi.aims.store.*;
 
@@ -10,22 +8,22 @@ public class StoreTest {
         Store store = new Store();
 
         // Create some DVDs
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King");
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars");
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin");
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc(0, "The Lion King", null, null, 0, 0);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc(1, "Star Wars", null, null, 0, 0);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc(2, "Aladdin", null, null, 0, 0);
 
         // Add DVDs to the store
-        store.addDVD(dvd1);
-        store.addDVD(dvd2);
+        store.addMedia(dvd1);
+        store.addMedia(dvd2);
 
         // Display DVDs in the store
         store.displayStore();
 
         // Remove a DVD from the store
-        store.removeDVD(dvd1);
+        store.removeMedia(dvd1);
 
         // Try to remove a DVD that is not in the store
-        store.removeDVD(dvd3);
+        store.removeMedia(dvd3);
 
         // Display DVDs in the store again
         store.displayStore();
