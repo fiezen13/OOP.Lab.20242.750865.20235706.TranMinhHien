810b6452a964416acb2343af24709e2e27340eee
fiezen13 <hawkins1302@gmail.com>
2025-05-19 12:36:51 +0700

    finish 3.JavaFX API

---
+++ lab03/GUIProject/src/module-info.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module GUIProject {
+	 requires javafx.controls;
+	 requires javafx.fxml;
+	 requires javafx.graphics; 
+	 requires javafx.media;
+	 requires javafx.web;
+	 requires javafx.swing;
+	 requires javafx.base;
+	 exports hust.soict.hedspi.javafx;
+}
